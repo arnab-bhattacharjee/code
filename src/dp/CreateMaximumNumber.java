6b711295095652810b215a6bf378950b53913d75
arnab <arnabbhattacharjeee@gmail.com>
2018-12-30 01:36:11 +0530

    DP problems

--- src/dp/CreateMaximumNumber.java
+++ src/dp/CreateMaximumNumber.java
@@ -16,15 +16,26 @@ public class CreateMaximumNumber {
             for (int i = nums1.length; i >= 0; i--) {
                 for (int j = nums2.length; j >= 0; j--) {
                     if (nums1.length - i + nums2.length - j < l) continue;
-                    int[] n1 = null;
-                    if (i + 1 <= nums1.length)
-                        n1 = DP[(l - 1)%2][i + 1][j].clone();
-                    int[] n2 = null;
-                    if (j + 1 <= nums2.length)
-                        n2 = DP[(l - 1)%2][i][j + 1].clone();
-                    if (n1 != null) n1[k - l] = nums1[i];
-                    if (n2 != null) n2[k - l] = nums2[j];
-                    DP[l%2][i][j] = max(n1, n2,l);
+                    int[] n = null;
+                    if (i+1 <= nums1.length && j+1 <= nums2.length) {
+                        if (nums1[i] > nums2[j]) {
+                            n = DP[(l-1)%2][i+1][j].clone();
+                            n[k-l] = nums1[i];
+                        } else if (nums1[i] < nums2[j]) {
+                            n = DP[(l-1)%2][i][j+1].clone();
+                            n[k-l] = nums2[j];
+                        } else {
+                            n = max(DP[(l-1)%2][i+1][j], DP[(l-1)%2][i][j+1], l-1).clone();
+                            n[k-l] = nums1[i];
+                        }
+                    } else if (i + 1 <= nums1.length) {
+                        n = DP[(l - 1) % 2][i + 1][j].clone();
+                        n[k-l] = nums1[i];
+                    } else if (j + 1 <= nums2.length) {
+                        n = DP[(l - 1) % 2][i][j + 1].clone();
+                        n[k-l] = nums2[j];
+                    }
+                    DP[l%2][i][j] = n;
                     if (i + 1 <= nums1.length)
                         DP[l%2][i][j] = max(DP[l%2][i][j], DP[l%2][i + 1][j],l);
                     if (j + 1 <= nums2.length)
